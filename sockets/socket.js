c59481a2e2c6bf26f8dd9b73a65c800203f362d6
Julio <julio@MacBook-Air-de-Julio.local>
2021-03-15 14:13:27 -0600

    BackendTerminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,17 +1,43 @@
 const { io } = require('../index');
+const Bands = require('../models/Bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+bands.addBand(new Band( 'Queen'));
+bands.addBand(new Band( 'Heroes del Silencio'));
+bands.addBand(new Band( 'Guns and Roses'));
+bands.addBand(new Band( 'Metallica'));
 
 
+io.on("connection", client => {
+    console.log("New device connected!")
 
-io.on('connection', client => {
-    console.log('Cliente Conectado');
+    client.emit('activeBands', bands.getBands());
 
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+    // print in the console when some device disconnects
+    client.on("disconnect", data => {
+        console.log("disconnected!")
+    })
+
+    // client.on('emitir-mensaje',(payload) => {
+    //     client.broadcast.emit('nuevo-mensaje', payload);
+    // });
+
+    client.on('vote-band', (payload) => {
+     
+        bands.voteBand(payload.id);
+        io.emit('activeBands', bands.getBands());
     });
 
-    client.on('mensaje', (payload) => {
-        console.log('Mensaje', payload);
-
-        io.emit('mensaje', { admin: 'Nuevo Mensaje' });
+    //Escuchar add-band
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('activeBands', bands.getBands());
     });
-});
+    //delte band
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('activeBands', bands.getBands());
+    }); 
+})
